05e64e20134792875c5a962906aa3b8092260765
Stefan Klinger <git@stefan-klinger.de>
2025-05-20 22:55:03 +0200

    initial

---
+++ README.md
@@ -0,0 +1,2 @@
+Playing with Rust's *trait objects*, which seem similar to Haskell's
+existential types.
---
+++ src/main.rs
@@ -0,0 +1,113 @@
+
+struct A {}
+
+impl A {
+    fn only_a(&self) {
+        println!("only A");
+    }
+}
+
+
+struct B {}
+
+impl B {
+    fn only_b(&self) {
+        println!("only B");
+    }
+}
+
+
+
+trait Printable {
+    fn print(&self);
+}
+
+impl Printable for A {
+    fn print(&self) {
+        println!("print A");
+    }
+}
+
+impl Printable for B {
+    fn print(&self) {
+        println!("print B");
+    }
+}
+
+fn print_list(items: &Vec<&dyn Printable>) {
+    for i in items {
+        i.print();
+    }
+}
+
+
+
+enum Discovery<'a> {
+    IsA(&'a A),
+    IsB(&'a B)
+}
+use Discovery::*;
+
+trait Discoverable {
+    fn discover(&self) -> Discovery;
+}
+
+impl Discoverable for A {
+    fn discover(&self) -> Discovery { IsA(&self) }
+}
+
+impl Discoverable for B {
+    fn discover(&self) -> Discovery { IsB(&self) }
+}
+
+trait CombinedTrait: Printable + Discoverable {}
+
+impl<T: Printable + Discoverable> CombinedTrait for T {}
+
+fn print_and_discover<T: CombinedTrait>(x: &T) {
+    x.print();
+    match x.discover() {
+        IsA(a) => a.only_a(),
+        IsB(b) => b.only_b(),
+    }
+}
+
+
+
+fn main() {
+
+    let a = A{};
+    let b = B{};
+
+
+    // Nice!
+
+    println!("-- different types in one list");
+    let items: Vec<&dyn Printable> = vec!(&a, &b);
+    print_list(&items);
+
+
+    // Quality!!!
+
+    println!("-- reconstruction of original type");
+    print_and_discover(&a);
+    print_and_discover(&b);
+
+
+    // Huh ?
+
+    println!("-- combined trait in vector (commented out)");
+
+    #[allow(unused_variables)]
+    let items: Vec<&dyn CombinedTrait> = vec!(&a, &b);
+
+    /* switching comment for broken code
+    print_list(&items);
+    // */
+
+    /* switching comment for broken code
+    for i in items {
+        print_and_discover(&i);
+    }
+    // */
+}
